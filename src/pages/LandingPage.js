d4320071b0f5f095057ebb4e778d44441f163081
Farhan <44315981+mrfarhan00@users.noreply.github.com>
2022-05-08 15:57:44 +0700

    Add Footer and Testimony
    Add Footer and Testimony

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,8 @@ import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
 import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer"
 
 import landingPage from "json/landingPage.json";
 export default class LandingPage extends Component {
@@ -14,10 +16,12 @@ export default class LandingPage extends Component {
   render() {
     return (
       <>
-        <Header {...this.props}> </Header>
+        <Header {...this.props}></Header>
         <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
         <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
         <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
+        <Footer />
       </>
     );
   }
--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -1,5 +1,5 @@
 //numberformat titik jika lebih dari seribu
 export default number => {
-    const formatNumbering = new Intl.NumberFormat("id-ID");
-      return formatNumbering.format(number);
-  }
+  const formatNumbering = new Intl.NumberFormat("id-ID");
+  return formatNumbering.format(number);
+}
